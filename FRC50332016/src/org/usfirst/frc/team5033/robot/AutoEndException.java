033c529abedea9b1edb10af1b35b8b683d73209d
curtis0gj <curtis.0gj@gmail.com>
2016-03-30 22:39:14 -0400

    lots of changes day b4 comp

---
+++ FRC50332016/src/org/usfirst/frc/team5033/robot/AutoEndException.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team5033.robot;
+
+public class AutoEndException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public AutoEndException() {
+		super();
+	}
+}
